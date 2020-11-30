ab9ab3f754e7ac318264cc508652671614f9e4ef
Igor Ribozavr <Igo-nikolaenk@yandex.ru>
2020-11-30 05:23:28 +0300

    arr deleted

--- src/components/Panel/components/ListFlights/ContentFlightsList/Like/Like.js
+++ src/components/Panel/components/ListFlights/ContentFlightsList/Like/Like.js
@@ -3,20 +3,19 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Favorite from '@material-ui/icons/Favorite';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './like.css';
 
-let arr = [];
-
 function Like() {
+  const data = useSelector((state) => state.panelReduser.count);
   const dispatch = useDispatch();
   const [state, setState] = useState({
     checkedA: true,
   });
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
-    event.target.checked ? arr.push(event.target.checked) : arr.pop();
-    dispatch({ type: 'COUNT', payload: arr });
+    event.target.checked ? data.push(event.target.checked) : data.pop();
+    dispatch({ type: 'COUNT', payload: data });
   };
 
   return (
